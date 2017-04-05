a24eae4cf6dfd51bf7f9c7db71307303aaf8df51
eileen <emcnaughton@wikimedia.org>
2017-04-06 11:34:42 +1200

    Alternate approach for issues with running tests on different version combos, #18

--- test/tests/BaseTestClass.php
+++ test/tests/BaseTestClass.php
@@ -9,6 +9,14 @@ use PHPUnit\Framework\TestCase;
 if (class_exists('\PHPUnit\Framework\TestCase')) {
     class BaseTestClass extends TestCase
     {
+        public function expectException($exception)
+        {
+            if (is_callable('parent::expectException')) {
+                parent::expectException($exception);
+            } else {
+                $this->setExpectedException($exception);
+            }
+        }
     }
 } elseif (class_exists('\PHPUnit_Framework_TestCase')) {
     class BaseTestClass extends PHPUnit_Framework_TestCase
