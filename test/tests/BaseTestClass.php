affcdafec07500a52d554108623ed93f65500f0b
Gabriel Bull <me@gabrielbull.com>
2017-05-15 07:56:09 -0400

    Added check on php version for tests

--- test/tests/BaseTestClass.php
+++ test/tests/BaseTestClass.php
@@ -5,7 +5,15 @@ namespace Omnimail\Tests;
 use PHPUnit_Framework_TestCase;
 use PHPUnit\Framework\TestCase;
 
-if (class_exists('\PHPUnit\Framework\TestCase')) {
+if (class_exists('\PHPUnit_Framework_TestCase') && stripos(phpversion(), '5') === 0) {
+    class BaseTestClass extends PHPUnit_Framework_TestCase
+    {
+        public function expectException($exception)
+        {
+            $this->setExpectedException($exception);
+        }
+    }
+} else if (class_exists('\PHPUnit\Framework\TestCase')) {
     class BaseTestClass extends TestCase
     {
         public function expectException($exception)
@@ -15,14 +23,6 @@ if (class_exists('\PHPUnit\Framework\TestCase')) {
             } else {
                 $this->setExpectedException($exception);
             }
-        }
-    }
-} elseif (class_exists('\PHPUnit_Framework_TestCase')) {
-    class BaseTestClass extends PHPUnit_Framework_TestCase
-    {
-        public function expectException($exception)
-        {
-            $this->setExpectedException($exception);
         }
     }
 }
