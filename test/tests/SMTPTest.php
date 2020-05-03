3668340df6ebf6a6caee9c0413003479572e9db1
Shaharia Azam <shaharia.azam@gmail.com>
2020-05-03 21:24:35 +0600

    SMTP can now support Attachment

--- test/tests/SMTPTest.php
+++ test/tests/SMTPTest.php
@@ -2,6 +2,7 @@
 
 namespace Omnimail\Tests;
 
+use Omnimail\Attachment;
 use Omnimail\Email;
 use Omnimail\Exception\Exception;
 use Omnimail\SMTP;
@@ -18,11 +19,17 @@ class SMTPTest extends BaseTestClass
 
         $sender = new SMTP($smtpHost, $username, $password, ['timeout' => 10]);
 
+        $attachment = new Attachment();
+        $attachment->setName('my_file.txt');
+        $attachment->setMimeType('text/plain');
+        $attachment->setContent('This is plain text');
+
         $email = (new Email())
             ->addTo('your@email.com')
             ->setFrom('from@email.com')
             ->setSubject('Hello, world!')
-            ->setTextBody('Hello World! How are you?');
+            ->setTextBody('Hello World! How are you?')
+            ->addAttachment($attachment);
 
         $sender->send($email);
     }
